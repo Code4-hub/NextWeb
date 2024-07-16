decd6326e6c910404cb7d37a371a20d154671f18
laabibi abdellah <admirablea42@gmail.com>
2024-07-16 15:39:55 +0100

    delete some unimportant cmnts

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,9 @@ import Blog from "./app/Blog/Blog";
 import About from "./app/About us/About";
 import Pricing from "./app/Pricing/Pricing";
 import Showcase from "./app/Showcase/Showcase";
-// import { Navbar } from "../pageComponents/Navbar";
-// import { FAQs } from "./FAQs/FAQs";
-// import { Footer } from "../pageComponents/Footer";
 import "./App.css";
 import { Navbar } from "./app/pageComponents/Navbar";
 import { Footer } from "./app/pageComponents/Footer";
-// import { FAQs } from "./app/Pricing/FAQs/FAQs";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/app/Pricing/PricingTabs.tsx
+++ src/app/Pricing/PricingTabs.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Badge } from "@/components/ui/badge";
 import { Monthly } from "./plan/Monthly";
