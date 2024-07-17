e48d2630e30579821d62879a309339a7793281e1
laabibi abdellah <admirablea42@gmail.com>
2024-07-17 20:28:53 +0100

    nothing

--- src/app/pageComponents/Navbar.tsx
+++ src/app/pageComponents/Navbar.tsx
@@ -1,6 +1,6 @@
 import { SiDevexpress } from "react-icons/si";
 import LetterPullup from "@/components/magicui/letter-pullup";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { MobileNavBar } from "./MobileNavBar";
 
